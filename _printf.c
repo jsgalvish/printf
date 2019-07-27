5dcb015f513275abae66a4fa605bab0fc1af112c
JS Galvis <jsgalvish@unal.edu.co>
2019-07-26 20:51:21 -0500

    initialize files

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
+int _printf(const char *format, ...)
+{
+	return (0);
+}
---
+++ holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+int _printf(const char *format, ...);
+
+#endif
