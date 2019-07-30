215f514274f4f56000595fa72c6ece066b08b4d5
Santiago Yanguas <santitoya-2001@outlook.com>
2019-07-30 15:51:24 +0000

    HOTFIX the brackets

--- 3-binary.c
+++ 3-binary.c
@@ -8,33 +8,33 @@
 
 int print_b(va_list binary_list)
 {
-	unsigned int i, count, Num, binary;
-	unsigned int arr[32];
+	unsigned int i, count, Num, binary, arr[32];
 
-	i = 0;
-	count = 0;
+	i = 0, count = 0;
 	Num = va_arg(binary_list, int);
-	binary = 0;
 
 	if (Num < 1)
+	{
 		_putchar(48);
 		count++;
 		return (count);
-
+	}
 	else
+	{
 		while (Num > 0)
+		{
 			binary = Num % 2;
 			Num /= 2;
 			arr[count] = binary;
 			count++;
-
+		}
 		i = count - 1;
-
 		while (i > 0)
+		{
 			_putchar('0' + arr[i]);
 			i--;
-
+		}
 		_putchar('0' + arr[i]);
-
+	}
 	return (count);
 }
